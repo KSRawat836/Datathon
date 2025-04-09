8d1985b47372b3d64dd45c3b507a1cb66d31d515
utkarsha-kempinmath <utkarshakempinmath1725@gmail.com>
2025-04-10 02:51:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home.jsx'
+import { useForm } from "react-hook-form"
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting } } = useForm();
+
+    const navigate = useNavigate(); 
+
+  const onSubmit = async (data) => {
+    console.log("Form submitted:", data);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    navigate('/Home', { state: data });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='attendity'> Attendity</h1>
+      {isSubmitting && <div>Loading...</div>}
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <input {...register("email", {
+            required: { value: true, message: "Email is required" }
+          })} placeholder='Your college email id' type="email" id="" />
+
+        <input {...register("password", {
+            required: { value: true, message: "Password is required" }
+          })} placeholder='Your password' type="password" />
+
+        <input disabled={isSubmitting} type="submit" value="Submit"  />
+      </form>
     </>
   )
 }
